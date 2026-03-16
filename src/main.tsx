b9b646b4223ea44fee1d176d43ea15b3a7ad1a2d
Ethan Breinholt <ethan.breinholt@nexhealth.com>
2026-03-16 15:51:30 -0600

    Removed unused TanStack React Query package

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom/client';
 import NotificationContextProvider from './components/contexts/notification-context';
 import App from './app';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <NotificationContextProvider>
-        <App />
-      </NotificationContextProvider>
-    </QueryClientProvider>
+    <NotificationContextProvider>
+      <App />
+    </NotificationContextProvider>
   </React.StrictMode>
 );
