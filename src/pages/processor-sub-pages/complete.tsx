4495112f2ed6e3daf38e27ad30ba1a5d2b3fd4c2
Ethan Breinholt <ethan.breinholt@nexhealth.com>
2026-03-26 11:52:29 -0600

    Imrpove process complete page

--- src/pages/processor-sub-pages/complete.tsx
+++ src/pages/processor-sub-pages/complete.tsx
@@ -2,6 +2,7 @@ import ProcessorSubPage from './processor-sub-page';
 import useFileSystem from '../../hooks/useFileSystem';
 import { ProcessSubPageProps } from '../../types/process-sub-page-props';
 import Button from '../../components/button';
+import { BiCheckDouble, BiLinkExternal } from 'react-icons/bi';
 
 const Complete = (props: ProcessSubPageProps) => {
   const { revealFileOrDirectory } = useFileSystem();
@@ -18,24 +19,30 @@ const Complete = (props: ProcessSubPageProps) => {
   return (
     <ProcessorSubPage
       title="Process Complete"
+      titleIcon={<BiCheckDouble size={42} />}
       appActions={props.appActions}
       hideCancelButton
     >
       {path && (
         <>
           <p>
-            Your data has successully been obtained and was saved to the
+            Your analytics have successully been obtained and were saved to the
             following location:
           </p>
           <button
             onClick={() => revealFileOrDirectory(path)}
-            className="text-green-500"
+            className="text-green-500 text-left"
           >
-            {path}
+            {path}{' '}
+            <span className="inline-block text-sandstone-300">
+              <BiLinkExternal />
+            </span>
           </button>
         </>
       )}
-      <Button label="Done" onClick={finish} style="primary" />
+      <div className="w-fit mx-auto mt-4">
+        <Button label="Finish" onClick={finish} style="primary" />
+      </div>
     </ProcessorSubPage>
   );
 };
--- src/pages/processor-sub-pages/processor-sub-page.tsx
+++ src/pages/processor-sub-pages/processor-sub-page.tsx
@@ -1,10 +1,12 @@
 import { BiX } from 'react-icons/bi';
 import { useModalContext } from '../../components/contexts/modal-context';
 import { AppActions } from '../process';
+import React from 'react';
 
 interface ProcessorSubPageProps {
   children: React.ReactNode;
   title: string;
+  titleIcon?: React.ReactNode;
   description?: string;
   appActions: AppActions;
   hideCancelButton?: boolean;
@@ -30,8 +32,9 @@ const ProcessorSubPage = (props: ProcessorSubPageProps) => {
 
   return (
     <div className="w-full">
-      <h1 className="text-4xl font-bold mb-4 text-sandstone-300">
+      <h1 className="text-4xl font-bold mb-4 text-sandstone-300 flex items-center gap-2">
         {props.title}
+        {props.titleIcon}
       </h1>
       {props.description && <p className="mb-3">{props.description}</p>}
       <div>{props.children}</div>
